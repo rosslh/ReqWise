3d8bc996b1b8c10905cb4aa977a4e821ee5ea77b
Ross Hill <rosshill@protonmail.com>
2020-04-06 13:24:33 -0400

    Configure CORS

--- server/src/index.js
+++ server/src/index.js
@@ -8,40 +8,43 @@ const CORS = require("fastify-cors");
 require("dotenv").config();
 
 function fastifyKnexJS(fastify, opts, next) {
-    try {
-        const handler = knex(opts);
-        fastify.decorate("knex", handler);
-        next();
-    } catch (err) {
-        next(err);
-    }
+  try {
+    const handler = knex(opts);
+    fastify.decorate("knex", handler);
+    next();
+  } catch (err) {
+    next(err);
+  }
 }
 
-module.exports = function(fastify, opts, next) {
-    // This loads all plugins defined in plugins
-    // those should be support plugins that are reused
-    // through your application
-    fastify.register(AutoLoad, {
-        dir: path.join(__dirname, "plugins"),
-        options: Object.assign({}, opts)
-    });
+module.exports = function (fastify, opts, next) {
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "plugins"),
+    options: Object.assign({}, opts),
+  });
 
-    // This loads all plugins defined in services
-    // define your routes in one of these
-    fastify.register(AutoLoad, {
-        dir: path.join(__dirname, "services"),
-        options: Object.assign({}, opts)
-    });
+  // This loads all plugins defined in services
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "services"),
+    options: Object.assign({}, opts),
+  });
 
-    // This loads all hooks defined in hooks
-    fastify.register(AutoLoad, {
-        dir: path.join(__dirname, "hooks"),
-        options: Object.assign({}, opts)
-    });
+  // This loads all hooks defined in hooks
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, "hooks"),
+    options: Object.assign({}, opts),
+  });
 
-    fastify.register(CORS, {});
+  fastify.register(CORS, {
+    methods: ["GET", "PUT", "POST", "DELETE"],
+    origin: ["https://reqwise.web.app", "https://reqwise.firebaseapp.com/"],
+  });
 
-    fastify.register(fastifyPlugin(fastifyKnexJS, ">=0.30.0"), development);
+  fastify.register(fastifyPlugin(fastifyKnexJS, ">=0.30.0"), development);
 
-    next();
+  next();
 };
