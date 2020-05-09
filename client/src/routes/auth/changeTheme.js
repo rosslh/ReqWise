58b9b7180f96ee26b9e836ff6ddd285e6482d613
Ross Hill <ross@rosshill.ca>
2020-05-09 16:19:15 -0400

    Hopefully fix cookie issue

--- client/src/routes/auth/changeTheme.js
+++ client/src/routes/auth/changeTheme.js
@@ -1,7 +1,11 @@
 export function put(req, res) {
     const { darkModeEnabled } = req.body;
     req.session.user = { ...req.session.user, darkModeEnabled };
-
+    req.session.save((err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
     res.setHeader("Content-Type", "application/json");
 
     res.end('"success"');
