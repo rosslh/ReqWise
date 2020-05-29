82e51dd597ff5fc2f7cb8d94fdea2abb782cecff
Ross Hill <ross@rosshill.ca>
2020-05-29 14:50:58 -0400

    Try to improve firebase perf

--- client/index.js
+++ client/index.js
@@ -1,4 +1,22 @@
 const functions = require("firebase-functions");
 const { expressServer } = require("./__sapper__/build/server/server");
 
-exports.ssr = functions.https.onRequest(expressServer);
+const oneHour = 60 * 60
+const oneDay = oneHour * 24
+const oneWeek = oneDay * 7
+const oneMonth = oneWeek * 4
+
+expressServer.get('/', (request, response) => {
+    try {
+        response.set(
+            'Cache-Control',
+            `public, max-age=${oneWeek}, must-revalidate, s-maxage=${oneMonth}, proxy-revalidate, stale-while-revalidate=${oneDay}, stale-if-error=${oneWeek}`
+        )
+        response.send(expressServer)
+    } catch (error) {
+        console.error('error in Sapper SSR function: ', error)
+    }
+})
+
+
+exports.ssr = functions.runWith({ memory: '2GB', timeoutSeconds: 540 }).https.onRequest(expressServer);
