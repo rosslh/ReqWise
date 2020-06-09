1bcfc7ab08011469394dbac83bb49ceaffca0c2d
Ross Hill <ross@rosshill.ca>
2020-06-09 10:55:55 -0400

    Work on history modal

---
+++ client/src/components/HistoryEntry.svelte
@@ -0,0 +1,58 @@
+<script>
+  export let reqversion;
+
+  const viewDiff = () => {};
+</script>
+
+<style>
+  .entryWrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: var(--background2);
+    border-radius: 0.4rem;
+    overflow: hidden;
+    border: 0.1rem solid var(--borderColor);
+    padding: 2rem;
+    margin: 1rem 0;
+  }
+
+  button.viewDiff {
+    margin-top: 0;
+  }
+
+  .secondary {
+    color: var(--secondaryText);
+  }
+
+  .rationale {
+    font-weight: 500;
+  }
+
+  .bottom {
+    font-size: 1.4rem;
+  }
+</style>
+
+<div class="entryWrapper">
+  <div class="left">
+    <div class="top">
+      {#if reqversion.rationale}
+        <span class="rationale">{reqversion.rationale}</span>
+      {:else}
+        <span class="secondary">No rationale</span>
+      {/if}
+    </div>
+    <div class="bottom">
+      {reqversion.authorName}
+      <span class="secondary">&lt;{reqversion.authorEmail}&gt;</span>
+    </div>
+  </div>
+  <div class="right">
+    <button
+      class="viewDiff button button-small button-outline"
+      on:click={viewDiff}>
+      View
+    </button>
+  </div>
+</div>
--- client/src/components/ViewRequirementHistoryModal.svelte
+++ client/src/components/ViewRequirementHistoryModal.svelte
@@ -3,6 +3,8 @@
   import { stores } from "@sapper/app";
   import { get } from "../api.js";
   import Skeleton from "../components/Skeleton.svelte";
+  import HistoryEntry from "../components/HistoryEntry.svelte";
+
   export let id;
   const { session } = stores();
   let versions = get(
@@ -11,11 +13,12 @@
   );
 </script>
 
-{id}
 {#await versions}
   <Skeleton rows={2} />
 {:then result}
-  <pre>{result.length}</pre>
+  {#each result as reqversion}
+    <HistoryEntry {reqversion} />
+  {/each}
 {:catch error}
   <section class="contentWrapper">
     <p style="color: var(--red)">{error.message}</p>
