23c0909b4f09e8ab8141f9f5371f88840961d19b
Ross Hill <ross@rosshill.ca>
2020-06-12 13:58:13 -0400

    Add more valid file name characters

--- server/src/services/projects/index.js
+++ server/src/services/projects/index.js
@@ -351,7 +351,7 @@ module.exports = async function (fastify, opts) {
         .returning("id"))[0];
 
       if (fileName) {
-        const uploadedFileName = `${uuidv4()}-${fileName.replace(/[^a-zA-Z0-9_-]/g, '')}`; // remove illegal characters
+        const uploadedFileName = `${uuidv4()}-${fileName.replace(/[^a-zA-Z0-9_. -]/g, '')}`; // remove illegal characters
 
         const data = Buffer.from(file.replace(/^data:.*\/.*;base64,/, ''), 'base64');
 
