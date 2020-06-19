53cddb4f0ad04900ab2022e9d5a002bfdb87106b
Ross Hill <ross@rosshill.ca>
2020-06-19 16:41:38 -0400

    Fix tests to be more consistent

--- client/cypress/integration/teams.js
+++ client/cypress/integration/teams.js
@@ -1,12 +1,12 @@
 describe('Teams page', () => {
     beforeEach(() => {
         cy.visit('/teams')
+        cy.waitForSkeleton();
         cy.login();
     });
 
     it('has the correct content', () => {
         cy.contains('h1', 'My Teams')
-        cy.get('.skeletonWrapper').should('not.exist');
         cy.contains('a', "Test team (don't delete)");
     });
 
@@ -15,7 +15,7 @@ describe('Teams page', () => {
         cy.get("#teamName").click().type('test name');
         cy.get("#teamDesc").click().type('test description');
         cy.get(".submitButton").click();
-        cy.get('.loadingSpinner').should('not.exist');
+        cy.waitForSpinner();
         cy.contains("a", "test name");
         cy.contains("td", "test description");
     });
@@ -25,23 +25,27 @@ describe('Team page', () => {
     beforeEach(() => {
         cy.visit('/teams')
         cy.login();
+        cy.url().should('include', '/teams');
+        cy.waitForSkeleton();
         cy.contains('a', "test name").click();
-        cy.url().should('include', 'team/');
+        cy.url().should('include', '/team/');
     });
 
     it('can update name and description', () => {
         cy.get("#name").click().clear().type("new name");
         cy.get("#description").click().clear().type("new description");
         cy.get(".submitButton").click();
-        cy.get('.loadingSpinner').should('not.exist');
+        cy.waitForSpinner();
         cy.reload(); // ensure new data is persisted
         cy.get("#name").should('have.value', "new name");
         cy.get("#description").should('have.value', "new description");
 
+        cy.wait(3000); // wait for input interactive
+
         cy.get("#name").click().clear().type("test name");
         cy.get("#description").click().clear().type("test description");
         cy.get(".submitButton").click();
-        cy.get('.loadingSpinner').should('not.exist');
+        cy.waitForSpinner();
         cy.reload(); // ensure new data is persisted
         cy.get("#name").should('have.value', "test name");
         cy.get("#description").should('have.value', "test description");
@@ -60,7 +64,7 @@ describe('Team page', () => {
         cy.get("#deleteTeamButton").click();
         cy.url().should('include', 'teams');
         cy.contains('h1', 'My Teams')
-        cy.get(".skeletonWrapper").should('not.exist');
+        cy.waitForSkeleton();
         cy.get("body").find("a.teamLink").should('have.length', 1);
     });
 });
--- client/cypress/support/commands.js
+++ client/cypress/support/commands.js
@@ -1,3 +1,4 @@
+import 'cypress-wait-until';
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -12,22 +13,28 @@
 // -- This is a parent command --
 Cypress.Commands.add("login", () => {
     cy.get("#email").click().type('test@reqwise.com');
-    cy.get("#email").should('have.value', "test@reqwise.com");
     cy.get("#pwd").click().type('1234');
-    cy.get("#pwd").should('have.value', "1234");
     cy.get(".submitButton").click();
-    cy.get('.loadingSpinner').should('not.exist');
+    cy.waitForSpinner(15000); // timeout after 15 seconds
     cy.url().should('not.include', 'login');
 });
 
 Cypress.Commands.add("goToProject", () => {
     cy.visit('/teams')
     cy.login();
+    cy.waitForSkeleton();
     cy.contains('a', "Test team (don't delete)").click();
     cy.url().should('include', 'team/');
+    cy.waitForSkeleton();
     cy.contains('a', "Test project (don't delete)").click();
     cy.url().should('include', 'project/');
 });
+
+
+
+Cypress.Commands.add("waitForSpinner", (timeout = 10000) => cy.waitUntil(() => cy.get('.loadingSpinner').should('not.be.visible'), { timeout }));
+Cypress.Commands.add("waitForSkeleton", (timeout = 10000) => cy.waitUntil(() => cy.get('.skeletonWrapper').should('not.be.visible'), { timeout }));
+
 //
 //
 // -- This is a child command --
