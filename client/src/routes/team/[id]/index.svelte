33b3bc8dc3dc7f605fc21aa3e5df5d2e565f526c
Ross Hill <ross@rosshill.ca>
2020-06-24 21:19:04 -0400

    add missing env vars

--- client/src/routes/team/[id]/index.svelte
+++ client/src/routes/team/[id]/index.svelte
@@ -262,18 +262,16 @@
   {#if isAdmin}
     <h2>Integrations</h2>
     <div class="panel compact">
-      {#if typeof window !== 'undefined'}
-        <a
-          href={`https://slack.com/oauth/v2/authorize?redirect_uri=${encodeURIComponent(`https://reqwise.com/team/${id}/slack/confirm`)}&scope=${process.env.REQWISE_SLACK_SCOPES}&client_id=${process.env.REQWISE_SLACK_CLIENT_ID}`}>
-          <img
-            alt="Add to Slack"
-            height="40"
-            width="139"
-            src="https://platform.slack-edge.com/img/add_to_slack.png"
-            srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x,
-            https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
-        </a>
-      {/if}
+      <a
+        href={`https://slack.com/oauth/v2/authorize?redirect_uri=${encodeURIComponent(`https://reqwise.com/team/${id}/slack/confirm`)}&scope=${process.env.REQWISE_SLACK_SCOPES}&client_id=${process.env.REQWISE_SLACK_CLIENT_ID}`}>
+        <img
+          alt="Add to Slack"
+          height="40"
+          width="139"
+          src="https://platform.slack-edge.com/img/add_to_slack.png"
+          srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x,
+          https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" />
+      </a>
     </div>
     <h2>Danger Zone</h2>
     <div class="panel compact">
