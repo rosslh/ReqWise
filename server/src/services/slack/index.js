422b3fd4911c5af78e891322d1e5c482578b0ca1
Ross Hill <ross@rosshill.ca>
2020-07-13 12:04:30 -0400

    Work on slash commands and modals

--- server/src/services/slack/index.js
+++ server/src/services/slack/index.js
@@ -101,38 +101,227 @@ module.exports = async (fastify, opts) => {
                             "type": "section",
                             "text": {
                                 "type": "mrkdwn",
-                                "text": "Create a feature: `/reqwise create feature`"
+                                "text": "Create a feature: `/reqwise new feature`"
                             }
                         },
                         {
                             "type": "section",
                             "text": {
                                 "type": "mrkdwn",
-                                "text": "Create a quality attribute: `/reqwise create qa`"
+                                "text": "Create a quality attribute: `/reqwise new qa`"
                             }
                         },
                         {
                             "type": "section",
                             "text": {
                                 "type": "mrkdwn",
-                                "text": "Create a business requirement group: `/reqwise create br`"
+                                "text": "Create a business requirement group: `/reqwise new br`"
                             }
                         },
                         {
                             "type": "section",
                             "text": {
                                 "type": "mrkdwn",
-                                "text": "Create a requirement: `/reqwise create requirement`"
+                                "text": "Create a requirement: `/reqwise new req`"
                             }
                         },
                     ]
                 });
             }
-            else if (request.body.text === "create") {
-                return "TODO: implement this"; // TODO
+            else if (request.body.text.startsWith("new feature")) {
+                const { slackAccessToken } = await fastify.knex.from("team").select("*").where({ "slackTeamId": request.body.team_id }).first();
+                const modal = {
+                    ...request.body,
+                    token: slackAccessToken,
+                    view: JSON.stringify({
+                        type: 'modal',
+                        title: {
+                            type: 'plain_text',
+                            text: 'Submit a new feature'
+                        },
+                        callback_id: 'interaction',
+                        submit: {
+                            type: 'plain_text',
+                            text: 'Submit'
+                        },
+                        blocks: [
+                            {
+                                block_id: 'title_block',
+                                type: 'input',
+                                label: {
+                                    type: 'plain_text',
+                                    text: 'Feature name'
+                                },
+                                element: {
+                                    action_id: 'title',
+                                    type: 'plain_text_input',
+                                },
+                            }
+                        ]
+                    })
+                };
+
+                await fastify.slack.views.open(modal);
+                return "";
             }
-            // console.log(request.body);
-            return ["success"];
+
+            else if (request.body.text.startsWith("new req")) {
+                const { slackAccessToken } = await fastify.knex.from("team").select("*").where({ "slackTeamId": request.body.team_id }).first();
+                const reqgroups = await fastify.knex.from("team")
+                    .select("*", "project.name as projectName", "reqgroup.name as name", "per_project_unique_id.readable_id as ppuid", "reqgroup.id as id")
+                    .where({ "slackTeamId": request.body.team_id })
+                    .join("project", "project.team_id", "team.id")
+                    .join("reqgroup", 'reqgroup.project_id', "project.id")
+                    .join("per_project_unique_id", "per_project_unique_id.id", "reqgroup.ppuid_id");
+                const modal = {
+                    ...request.body,
+                    token: slackAccessToken,
+                    view: JSON.stringify({
+                        type: 'modal',
+                        title: {
+                            type: 'plain_text',
+                            text: 'Submit a new requirement'
+                        },
+                        callback_id: 'interaction',
+                        submit: {
+                            type: 'plain_text',
+                            text: 'Submit'
+                        },
+                        blocks: [
+                            {
+                                block_id: 'title_block',
+                                type: 'input',
+                                label: {
+                                    type: 'plain_text',
+                                    text: 'Requirement description'
+                                },
+                                element: {
+                                    action_id: 'title',
+                                    type: 'plain_text_input',
+                                },
+                            },
+                            {
+                                block_id: 'priority_block',
+                                type: 'input',
+                                label: {
+                                    type: 'plain_text',
+                                    text: 'Priority'
+                                },
+                                element: {
+                                    action_id: 'priority',
+                                    type: 'static_select',
+                                    options: [
+                                        {
+                                            text: {
+                                                type: "plain_text",
+                                                text: "High"
+                                            },
+                                            value: "high"
+                                        },
+                                        {
+                                            text: {
+                                                type: "plain_text",
+                                                text: "Medium"
+                                            },
+                                            value: "medium"
+                                        },
+                                        {
+                                            text: {
+                                                type: "plain_text",
+                                                text: "Low"
+                                            },
+                                            value: "low"
+                                        }
+                                    ]
+                                },
+                                optional: false
+                            },
+                            {
+                                block_id: 'status_block',
+                                type: 'input',
+                                label: {
+                                    type: 'plain_text',
+                                    text: 'Status'
+                                },
+                                element: {
+                                    action_id: 'status',
+                                    type: 'static_select',
+                                    options: [
+                                        {
+                                            text: {
+                                                type: "plain_text",
+                                                text: "Proposed"
+                                            },
+                                            value: "proposed"
+                                        },
+                                        {
+                                            text: {
+                                                type: "plain_text",
+                                                text: "Accepted"
+                                            },
+                                            value: "accepted"
+                                        },
+                                        {
+                                            text: {
+                                                type: "plain_text",
+                                                text: "Implemented"
+                                            },
+                                            value: "implemented"
+                                        }
+                                    ]
+                                },
+                                optional: false
+                            },
+                            {
+                                block_id: 'Reqgroup_block',
+                                type: 'input',
+                                label: {
+                                    type: 'plain_text',
+                                    text: 'Requirement group'
+                                },
+                                element: {
+                                    action_id: 'Reqgroup',
+                                    type: 'static_select',
+                                    options: reqgroups.map(g => ({
+                                        text: {
+                                            type: "plain_text",
+                                            text: `#${g.ppuid} - ${g.name} (${g.projectName})`
+                                        },
+                                        value: String(g.id)
+                                    }))
+                                },
+                                optional: false
+                            }
+                        ]
+                    })
+                };
+
+                await fastify.slack.views.open(modal);
+                return "";
+            }
+
+            return "Command not recognized";
         }
     );
+
+    const postInteractionSchema = {
+        queryString: {},
+        params: {},
+        headers: {
+            type: "object",
+            properties: {
+                "Content-Type": { type: "string" },
+            },
+            required: ["Content-Type"],
+        },
+        response: {},
+    };
+    fastify.post(
+        "/interaction",
+        { schema: postInteractionSchema },
+        async (request, reply) => {
+            const data = JSON.parse(request.body.payload);
+            console.log(data);
+            return ""; // TODO: finish this
+        });
 };
