7aa3f26694126b4f879bd13fceda03676c6ac7d3
Ross Hill <ross@rosshill.ca>
2020-07-13 14:43:51 -0400

    Can now add features over slack

--- server/src/services/slack/index.js
+++ server/src/services/slack/index.js
@@ -130,6 +130,10 @@ module.exports = async (fastify, opts) => {
             }
             else if (request.body.text.startsWith("new feature")) {
                 const { slackAccessToken } = await fastify.knex.from("team").select("*").where({ "slackTeamId": request.body.team_id }).first();
+                const projects = await fastify.knex.from("team")
+                    .select("*", "project.name as name", "project.id as id")
+                    .where({ "slackTeamId": request.body.team_id })
+                    .join("project", "project.team_id", "team.id");
                 const modal = {
                     ...request.body,
                     token: slackAccessToken,
@@ -139,23 +143,65 @@ module.exports = async (fastify, opts) => {
                             type: 'plain_text',
                             text: 'Submit a new feature'
                         },
-                        callback_id: 'interaction',
+                        callback_id: 'new_feature',
                         submit: {
                             type: 'plain_text',
                             text: 'Submit'
                         },
                         blocks: [
                             {
-                                block_id: 'title_block',
+                                block_id: 'name_block',
                                 type: 'input',
                                 label: {
                                     type: 'plain_text',
                                     text: 'Feature name'
                                 },
                                 element: {
-                                    action_id: 'title',
+                                    action_id: 'name',
                                     type: 'plain_text_input',
                                 },
+                            },
+                            {
+                                block_id: 'project_block',
+                                type: 'input',
+                                label: {
+                                    type: 'plain_text',
+                                    text: 'Project'
+                                },
+                                element: {
+                                    action_id: 'project',
+                                    type: 'static_select',
+                                    options: projects.map(p => ({
+                                        text: {
+                                            type: "plain_text",
+                                            text: p.name
+                                        },
+                                        value: String(p.id)
+                                    }))
+                                },
+                                optional: false
+                            },
+                            {
+                                block_id: 'prioritizable_block',
+                                "type": "input",
+                                "element": {
+                                    action_id: "is_prioritizable",
+                                    "type": "checkboxes",
+                                    "options": [
+                                        {
+                                            "text": {
+                                                "type": "plain_text",
+                                                "text": "Requirements are prioritizable"
+                                            },
+                                            "value": "prioritizable"
+                                        }
+                                    ]
+                                },
+                                "label": {
+                                    "type": "plain_text",
+                                    "text": "Options"
+                                },
+                                optional: true
                             }
                         ]
                     })
@@ -182,21 +228,21 @@ module.exports = async (fastify, opts) => {
                             type: 'plain_text',
                             text: 'Submit a new requirement'
                         },
-                        callback_id: 'interaction',
+                        callback_id: 'new_requirement',
                         submit: {
                             type: 'plain_text',
                             text: 'Submit'
                         },
                         blocks: [
                             {
-                                block_id: 'title_block',
+                                block_id: 'description_block',
                                 type: 'input',
                                 label: {
                                     type: 'plain_text',
                                     text: 'Requirement description'
                                 },
                                 element: {
-                                    action_id: 'title',
+                                    action_id: 'description',
                                     type: 'plain_text_input',
                                 },
                             },
@@ -273,14 +319,14 @@ module.exports = async (fastify, opts) => {
                                 optional: false
                             },
                             {
-                                block_id: 'Reqgroup_block',
+                                block_id: 'reqgroup_block',
                                 type: 'input',
                                 label: {
                                     type: 'plain_text',
                                     text: 'Requirement group'
                                 },
                                 element: {
-                                    action_id: 'Reqgroup',
+                                    action_id: 'reqgroup',
                                     type: 'static_select',
                                     options: reqgroups.map(g => ({
                                         text: {
@@ -304,6 +350,16 @@ module.exports = async (fastify, opts) => {
         }
     );
 
+    const getReqgroupType = type => {
+        if (type === "quality") {
+            return "quality attribute";
+        } else if (type === "business") {
+            return "business requirement group";
+        } else {
+            return type;
+        }
+    }
+
     const postInteractionSchema = {
         queryString: {},
         params: {},
@@ -321,7 +377,75 @@ module.exports = async (fastify, opts) => {
         { schema: postInteractionSchema },
         async (request, reply) => {
             const data = JSON.parse(request.body.payload);
-            console.log(data);
-            return ""; // TODO: finish this
+            if (data.view.callback_id === "new_feature") {
+                const name = data.view.state.values.name_block.name.value;
+                const project_id = Number(data.view.state.values.project_block.project.selected_option.value);
+                const type = "feature";
+                const isPrioritized = !!data.view.state.values.prioritizable_block.is_prioritizable.selected_options;
+
+                const { slackAccessToken: token } = await fastify.knex
+                    .from("project")
+                    .select("team.*", "project.*")
+                    .join("team", "team.id", "project.team_id")
+                    .where("project.id", project_id)
+                    .first();
+
+                const { id: account_id, name: account_name, imageName: account_imageName } = await fastify.knex
+                    .from("slackUser")
+                    .select("account.*")
+                    .join("account", "account.slackUser_id", "slackUser.id")
+                    .where("slackUser.slackId", data.user.id).first();
+
+                const maxPpuid =
+                    (
+                        await fastify
+                            .knex("per_project_unique_id")
+                            .where({ project_id })
+                            .max("readable_id")
+                            .first()
+                    ).max || 0;
+
+                await fastify.knex("project").where({ id: project_id }).update({ reqgroups_updated_at: new Date(Date.now()) });
+
+                const ppuid_id = (await fastify
+                    .knex("per_project_unique_id")
+                    .insert({
+                        project_id,
+                        readable_id: maxPpuid + 1
+                    })
+                    .returning("id"))[0];
+
+                const [id] = await fastify
+                    .knex("reqgroup")
+                    .insert({
+                        project_id,
+                        name,
+                        type,
+                        ppuid_id,
+                        created_by: account_id,
+                        updated_by: account_id,
+                        isPrioritized
+                    })
+                    .returning("id");
+
+                if (token) {
+                    const channel = await fastify.slackGetChannelId(project_id);
+                    await fastify.slack.chat.postMessage({
+                        text: `${account_name || data.user.name} made a new ${getReqgroupType(type)}: <https://reqwise.com/project/${fastify.obfuscateId(project_id)}|#${maxPpuid + 1} - ${name}>.`,
+                        token,
+                        channel,
+                        username: account_name || data.user.name,
+                        icon_url: account_imageName && `https://storage.googleapis.com/user-file-storage/${account_imageName}`
+                    });
+                }
+
+                await fastify.createAlert("create", "reqgroup", name, id, project_id, account_id);
+
+            }
+            else if (data.view.callback_id === "new_requirement") {
+                console.log(data.view.state.values.description_block.description);
+                return "TODO";
+            }
+            return "";
         });
 };
