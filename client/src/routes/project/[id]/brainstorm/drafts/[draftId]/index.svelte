0587ddf2bf8273c19149f6eacc6e23aaf83d8c8f
Ross Hill <ross@rosshill.ca>
2020-07-14 15:39:34 -0400

    Can now create prompts for questionnaires

--- client/src/routes/project/[id]/brainstorm/drafts/[draftId]/index.svelte
+++ client/src/routes/project/[id]/brainstorm/drafts/[draftId]/index.svelte
@@ -10,8 +10,27 @@
 
 <script>
   import { get } from "../../../../../../api.js";
+  import { stores } from "@sapper/app";
+  import { modalContent, modalProps } from "../../../../../../stores.js";
+  import AddBrainstormPromptModal from "../../../../../../components/AddBrainstormPromptModal.svelte";
 
   export let questionnaire;
+
+  let prompts = questionnaire.prompts;
+
+  const { page } = stores();
+
+  $: updatePrompts = async () => {
+    ({ prompts } = await get(
+      `/questionnaires/${$page.params.draftId}`,
+      session.user && session.user.jwt
+    ));
+  };
+
+  $: addPrompt = () => {
+    modalContent.set(AddBrainstormPromptModal);
+    modalProps.set({ update: updatePrompts, id: $page.params.draftId });
+  };
 </script>
 
 <section class="contentWrapper">
@@ -19,5 +38,10 @@
     Draft questionnaire:
     <em>{questionnaire.description}</em>
   </h2>
-  <pre>{JSON.stringify(questionnaire, null, 2)}</pre>
+  <button on:click={addPrompt} class="button button-success">Add prompt</button>
+  <button class="button button-secondary button-outline">Settings</button>
+  <button class="button button-secondary button-outline">Publish</button>
+</section>
+<section class="contentWrapper">
+  {#each prompts as prompt (prompt.id)}{JSON.stringify(prompt)}{/each}
 </section>
--- server/src/services/questionnaires/index.js
+++ server/src/services/questionnaires/index.js
@@ -23,14 +23,84 @@ module.exports = async function (fastify, opts) {
             schema: getQuestionnaireSchema,
         },
         async function (request, reply) {
-            return await fastify.knex
+            const questionnaire = await fastify.knex
                 .from("brainstormForm")
                 .select("*")
-                .leftJoin("brainstormPrompt", "brainstormPrompt.brainstormForm_id", "brainstormForm.id")
                 .where({
                     "brainstormForm.id": request.params.questionnaireId,
                 })
                 .first();
+
+            const prompts = await fastify.knex.from("brainstormPrompt").select("*").where({
+                "brainstormForm_id": request.params.questionnaireId
+            });
+
+            return { ...questionnaire, prompts };
+        }
+    );
+
+    const postStakeholderSchema = {
+        body: {
+            type: "object",
+            required: ["prompt", "type"],
+            properties: {
+                prompt: { type: "string" },
+                type: { type: "string" },
+                minVal: { type: "number" },
+                maxVal: { type: "number" },
+                options: { type: "array", items: { type: "string" } }
+            }
+        },
+        queryString: {},
+        params: {
+            type: "object",
+            properties: {
+                questionnaireId: { type: "number" },
+            },
+        },
+        headers: {
+            type: "object",
+            properties: {
+                Authorization: { type: "string" },
+                "Content-Type": { type: "string" },
+            },
+            required: ["Authorization", "Content-Type"],
+        },
+        response: {},
+    };
+    fastify.post(
+        "/:questionnaireId/prompts",
+        {
+            preValidation: [fastify.authenticate, fastify.isTeamMember],
+            schema: postStakeholderSchema,
+        },
+        async function (request, reply) {
+            const { prompt, options, minVal, maxVal, type } = request.body;
+            const { questionnaireId } = request.params;
+
+            const [id] = await fastify
+                .knex("brainstormPrompt")
+                .insert({
+                    prompt,
+                    numericFloor: minVal,
+                    numericCeiling: maxVal,
+                    responseType: type,
+                    brainstormForm_id: questionnaireId
+                })
+                .returning("id");
+
+            if (options && options.length) {
+                await Promise.all(options.map(async option => {
+                    await fastify
+                        .knex("brainstormResponseOption")
+                        .insert({
+                            value: option,
+                            brainstormPrompt_id: id
+                        });
+                }));
+            }
+
+            return ["success"];
         }
     );
 };
