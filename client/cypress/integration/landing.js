27b14aba5e43d6edf84a91a257efa8dc19b67a15
Ross Hill <ross@rosshill.ca>
2020-09-11 20:58:48 -0400

    Fix failing test

--- client/cypress/integration/landing.js
+++ client/cypress/integration/landing.js
@@ -1,19 +1,19 @@
 describe('Landing page', () => {
-    beforeEach(() => {
-        cy.visit('/')
-    });
+  beforeEach(() => {
+    cy.visit('/')
+  });
 
-    it('has the correct <h1>', () => {
-        cy.contains('h1', 'ReqWise')
-    });
+  it('has the correct slogan', () => {
+    cy.contains('p', "An easier way for digital agencies to understand their customer's needs.")
+  });
 
-    it('can navigate to login', () => {
-        cy.get('#loginLink').click();
-        cy.url().should('include', 'login');
-    });
+  it('can navigate to login', () => {
+    cy.get('#loginLink').click();
+    cy.url().should('include', 'login');
+  });
 
-    it('can navigate to signup', () => {
-        cy.get('#signupLink').click();
-        cy.url().should('include', 'sign-up');
-    });
+  it('can navigate to signup', () => {
+    cy.get('#signupLink').click();
+    cy.url().should('include', 'sign-up');
+  });
 });
