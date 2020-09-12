e4191fd36530c639a83d6e6c69e3fdeab4bf51a6
Ross Hill <ross@rosshill.ca>
2020-09-11 20:17:15 -0400

    Start work on landing page

---
+++ client/src/components/BrowserUi.svelte
@@ -0,0 +1,56 @@
+<style>
+  .browserWrapper {
+    perspective: 80rem;
+    margin: 3rem 0;
+  }
+  .browserUi {
+    border: 0.1rem solid var(--borderColor);
+    border-radius: 0.5rem;
+    overflow: hidden;
+    padding: 0.5rem;
+    background-color: var(--background1);
+    width: 44.444rem;
+    height: 25rem;
+    transform: rotateY(-20deg) scale(0.9);
+  }
+
+  .browserHeader {
+    background-color: #eee;
+    margin: -0.5rem -0.5rem 0;
+    padding: 0.75rem;
+    display: flex;
+    align-items: center;
+    margin-bottom: 1rem;
+  }
+
+  .browserWrapper .browserButton {
+    height: 1rem;
+    width: 1rem;
+    border-radius: 50%;
+    margin-right: 0.5rem;
+    opacity: 0.8;
+  }
+
+  .browserWrapper .browserButton.close {
+    background-color: var(--red);
+  }
+
+  .browserWrapper .browserButton.minimize {
+    background-color: var(--orange);
+  }
+
+  .browserWrapper .browserButton.maximize {
+    background-color: var(--green);
+  }
+</style>
+
+<div class="browserWrapper">
+  <div class="browserUi">
+    <div class="browserHeader">
+      <div class="browserButton close" />
+      <div class="browserButton minimize" />
+      <div class="browserButton maximize" />
+    </div>
+    <slot />
+  </div>
+</div>
--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,17 +1,36 @@
+<script>
+  import BrowserUi from "../components/BrowserUi.svelte";
+</script>
+
 <style>
-  p.first {
-    font-size: 2.5rem;
+  div.sloganSection {
+    background-color: var(--background2);
   }
 
-  p {
-    font-size: 2rem;
+  div.sloganSection .twoColumns {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  p.firstColumn {
+    font-size: 2.5rem;
+    font-weight: 700;
+    line-height: 6rem;
   }
 </style>
 
 <svelte:head>
   <title>ReqWise | Requirements Management</title>
 </svelte:head>
-<div class="contentWrapper">
-  <h1>ReqWise</h1>
-  <p class="first">The next-gen requirements management tool.</p>
+<div class="sloganSection">
+  <div class="contentWrapper twoColumns">
+    <p class="firstColumn">
+      An easier way for digital agencies<br /> to understand their customer's needs.
+    </p>
+    <div class="secondColumn">
+      <BrowserUi>Lmao</BrowserUi>
+    </div>
+  </div>
 </div>
+<div class="contentWrapper">Features</div>
