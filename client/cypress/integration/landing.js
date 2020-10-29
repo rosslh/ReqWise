7de5a0e4b374bb8302fb2d25a4304d3ac1b13100
Ross Hill <ross@rosshill.ca>
2020-10-29 12:07:55 -0400

    Fix failing test

--- client/cypress/integration/landing.js
+++ client/cypress/integration/landing.js
@@ -4,7 +4,7 @@ describe('Landing page', () => {
   });
 
   it('has the correct headings', () => {
-    cy.contains('h1', "An easier way for digital agencies to understand their clients' needs.");
+    cy.contains('h1', "An easier way for web developers and digital agencies to understand their clients' needs.");
     cy.contains('h2', "Brainstorm ideas with questionnaires");
     cy.contains('h2', "Collaborate with your team remotely");
     cy.contains('h2', "Organize and prioritize requirements");
